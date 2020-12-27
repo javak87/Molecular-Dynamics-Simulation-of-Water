c7873ecb96a810ba714515b072962f08e305955d
javak87 <javak87@mi.fu-berlin.de>
2020-12-27 19:09:56 +0330

    this file has been updated

--- cell_linked_list.py
+++ cell_linked_list.py
@@ -3,23 +3,25 @@ import math
 
 class CellLinked ():
 
-    def __init__ (self, box_len: float, r_cut: float) :
+    def __init__ (self, box_len: float, r_cut: float, postate : np.ndarray) :
         """
-        This class initialize with cubic simulation box (L*L*L).
-        Here, L=box_len and r_cut is a cut-off radius.
-        no_cells is number of cell per axis
+        Initialize parameters for identified neighbor cells and particles inside these cells
+
+            Parameters:
+                box_len (float) : cube simulation box dimension (box_len*box_len*box_len)
+                r_cut (float) : cut-off radius
+                no_cells (float) : number of cell per axis
+                postate (np.ndarray): position of particles in 3D
         """
         self.box_len = box_len
         self.r_cut = r_cut
         self.no_cells = math.floor (self.box_len/self.r_cut)
+        self.postate = postate
 
-    def __call__ (self, postate: np.ndarray) :
+    def __call__ (self) -> tuple:
 
         """ 
             Compute the head and list array using simulation box length, cut-off radius, and the position state.
-
-            Parameters:
-                postate (np.ndarray):       position of particles
 
             Returns
                 head_arr (list):   head array in the linked list (head_arr.shape==number of cells)
@@ -30,12 +32,14 @@ class CellLinked ():
         head_arr = [-1] * self.no_cells*self.no_cells*self.no_cells
 
         # Initialize a list array with -1, size = the total number of particles
-        list_arr = [-1] * postate.shape[0]
+        list_arr = [-1] * self.postate.shape[0]
 
-        for i in range (0, postate.shape[0]) :
+        print (self.postate[1,:])
+
+        for i in range (0, self.postate.shape[0]) :
 
             # calculate cell index of each particles based on the particle position
-            index = self._cellindex (postate[i,:])
+            index = self._cellindex (self.postate[i,:])
 
             # calculate list array
             list_arr [i] = head_arr [index]
@@ -49,19 +53,32 @@ class CellLinked ():
             
 
 
-    def _cellindex (self, postate) :
+    def _cellindex (self, one_particle_pos : np.ndarray) -> int :
 
         """ Calculate the cell index based on the i, j, and k index
+
+                Parameter :
+                    one_particle_pos (np.ndarray) : position of 1 particle (one_particle_pos.shape = (1,3))
+                
+                Return :
+                    cell's index (int) : cell index for each particles 
+
         """
-        i = math.floor (postate[0]/self.r_cut)
-        j = math.floor (postate[1]/self.r_cut)
-        k = math.floor (postate[2]/self.r_cut)
+        i = math.floor (one_particle_pos[0]/self.r_cut)
+        j = math.floor (one_particle_pos[1]/self.r_cut)
+        k = math.floor (one_particle_pos[2]/self.r_cut)
         return int (k+j*self.no_cells+i*self.no_cells*self.no_cells)
 
-    def _find_neighbor_index (self, cellind) :
+    def _find_neighbor_index (self, cellind: int) -> list:
 
         """
         calculate neighbor cell index based on the cell index
+            Parameter :
+                cellind (int) : cell index which is camputed by _cellindex method
+
+            Return :
+                neighbor_cell_inx (list) : neighbor cell indexes by the given cell index
+
         """
         #  calculate i, j, and k index based on the cell index
 
@@ -81,28 +98,74 @@ class CellLinked ():
                         neighbor_cell_inx.append(new_index)
         return neighbor_cell_inx
 
-    def _find_neighbor_cells (self, head_arr) :
+    def _find_neighbor_cells (self, head_arr: list) -> list:
 
         """
         calculate neighbor cells based on head array
+
+            Parameter :
+                head_arr (list) : a list of head array
+
+            Return :
+                neighbor_cells (list) : neighbor cell indexes
         """
 
-        cells = []
+        neighbor_cell_indexes = []
         for j in range (0, len (head_arr)) :           
             neighbor_cell_inx = self._find_neighbor_index (j)           
-            cells.append(neighbor_cell_inx)
+            neighbor_cell_indexes.append(neighbor_cell_inx)
 
-        return cells
+        return neighbor_cell_indexes
+    
+    def _find_particles_inside_neighbor_cells (self, list_arr : list, head_arr : list, neighbor_cell_indexes : list) -> np.ndarray:
+    
+        """
+        find particles in each cell and neighbor cells are identified
+            Parameters :
+                list_arr (list) : list array
+                head_arr (list) : head array
+                neighbor_cell_indexes (list) : neighbor cells indexes
 
-if __name__=="__main__":
+            Return :
+                temporary_state (np.ndarray) : state of all particles positioned on the given neighbor_cell_indexes
+        """
+        new_head_arr =[]
+        for i in neighbor_cell_indexes :
+            new_head_arr.append (head_arr[i])
+        temporary_state = np.zeros((1,3))
 
-    box_len=3
-    r_cut= 1
-    postate= np.random.randint(box_len, size=(20, 3))
-    model = CellLinked (box_len,  r_cut)
-    head_arr, list_arr = model (postate)
-    cells = model._find_neighbor_cells(head_arr)
-    print ("the head array is : \n ", head_arr)
-    print ("list array is : \n ", list_arr)
+        for ii in new_head_arr:
+            if ii > -1 :
+                cell_state = np.zeros((1,3))
+                cell_state = np.concatenate((cell_state, np.array ([self.postate[ii]])))
+                while  list_arr[ii] != -1 :
+                    cell_state = np.concatenate((cell_state, np.array ([self.postate[list_arr[ii]]])))
+                    ii = list_arr[ii]
+
+                cell_state = np.flip(cell_state[1:, :], 0)
+                temporary_state = np.concatenate((temporary_state, cell_state))
+        
+        
+        return temporary_state[1:, :]
+
+
+
+
+#if __name__=="__main__":
+
+#    box_len=3
+#    r_cut= 1
+#    postate = box_len * np.random.random_sample((10, 3))
+#    model = CellLinked (box_len,  r_cut, postate)
+#    head_arr, list_arr = model ()
+#    neighbor_cells = model._find_neighbor_cells(head_arr)
+#    neighbor_cells = neighbor_cells[13]
+#    temporary_state = model._find_particles_inside_neighbor_cells (list_arr, head_arr, neighbor_cells)
+
+#    mask = np.isin(postate, temporary_state)
+#    assert np.all(mask == True) == True, 'particles in neighbor cells has been indetified wrongly'
+
+#    print ("the head array is : \n ", head_arr)
+#    print ("list array is : \n ", list_arr)
 
     
