dac31dd8dd98c5b79bb55beb1a213b99c1153eed
javak87 <javak87@mi.fu-berlin.de>
2021-02-03 02:05:50 +0330

    The first version of main.py

--- main.py
+++ main.py
@@ -14,5 +14,42 @@ from FileOperation import FileOperation
 import h5py
 import sys
 from apply_periodic_boundary import PeriodicBoundary
+
 from visualization import Visualization
 from simulation import Simulation
+from simulation_visualization_inputs import *
+
+
+print ('------------\n')
+print ('The data for molecular simulation of water stored in the simulation_visualization_inputs.py \n')
+print ('------------\n')
+print ('You want to visualize the result stored in the hdf5 file or want to simulate the water molecules and after that see the visualization. \n')
+print ('if you want to simulate water molecules, this process might the time-consuming. \n')
+print ('------------\n')
+
+vis_or_sim = input ('Please insert "vis" for visualization and insert "sim" for simulation : \n')
+
+# Visualize the result stored in the hdf5 file
+if vis_or_sim == 'vis' :
+
+    viusl_obj = Visualization (timesteps, update_interval_animation, save_data_itr, hdf5_file_name, x_upper, y_upper, z_upper)
+    visual_result = viusl_obj()
+
+# Simulte and the visulize the result
+
+else :
+    sim =Simulation (grid, intmolecdist, hoh_angle,
+                    oh_len, box_len,
+                    O_mass, H_mass,
+                    Kb, temp, sigma,
+                    epsilon, r_cut, compmethod , k_b, tet_eq, k_tet, save_data_itr)
+    postate = sim ()
+
+# visulize the result
+
+    viusl_obj = Visualization (timesteps, update_interval_animation, save_data_itr, hdf5_file_name, x_upper, y_upper, z_upper)
+    visual_result = viusl_obj()
+
+print ('The package runs successfully\n')
+
+
