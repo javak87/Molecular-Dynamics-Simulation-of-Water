ea3f452a67e19d1678e9969cde50aa15b212b83a
Florian <florian.anders@fu-berlin.de>
2021-02-26 11:17:16 +0100

    moved python files into molecular_simulation_python_files

---
+++ molecular_simulation_python_files/Animation.py
@@ -0,0 +1,216 @@
+# 16.01.2021
+# Author: Florian Anders
+
+import matplotlib.animation as animation
+import mpl_toolkits.mplot3d.axes3d as axes3d
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from FileOperation import FileOperation as FileIO
+
+
+class Animation:
+
+    @staticmethod
+    def update_graph(step: int, maxstep: int, graph, array: np.ndarray, t_interval: int, axis, update_interval: int):
+
+        """
+        This method updates the animated scatter points
+
+        :param step: The step is the simulated timestep
+        :return:
+        """
+
+        graph._offsets3d = \
+            (
+                array[step][:][..., 0], array[step][:][..., 1], array[step][:][..., 2]
+            )
+
+        #axis.set_title\
+        plt.title\
+            (
+                "3D Animation of scatters representing atoms\n\nTimestep: {0} / {1} fs, "
+                "Time Interval: {2} fs\n\nUpdate Interval For Animation: {3} ms".format(step * t_interval, maxstep, t_interval, update_interval), loc='center', y=1.0)
+
+    @staticmethod
+    def define_colour_array(molecule_count: int, rgb_value_array_oxygen: np.ndarray, rgb_value_array_hydogen: np.ndarray):
+
+        """
+        This method assigns each atom type a specific colour.
+
+        :param molecule_count: number of molecules
+        :param rgb_value_array_oxygen: Array containing the desired RGB-values in the simulation for the oxygen atom
+        :param rgb_value_array_hydogen: Array containing the desired RGB-values in the simulation for the hydrogen atom
+        :return: Numpy-array of atom-specific colours
+        """
+
+        colour_array = np.zeros(shape=(molecule_count * 3, 3))
+
+        for i in range(molecule_count * 3):
+
+            if i % 3 == 0 or i == 0:
+
+                colour_array[i, 0] = rgb_value_array_oxygen[0]
+                colour_array[i, 1] = rgb_value_array_oxygen[1]
+                colour_array[i, 2] = rgb_value_array_oxygen[2]
+
+            else:
+
+                colour_array[i, 0] = rgb_value_array_hydogen[0]
+                colour_array[i, 1] = rgb_value_array_hydogen[1]
+                colour_array[i, 2] = rgb_value_array_hydogen[2]
+
+        return colour_array
+
+    @staticmethod
+    def define_atom_size_array(molecule_count: int, size_of_hydrogen: float, size_ratio_oxygen_hydrogen: float):
+
+        """
+        This method assigns each atom type a specific radius shown in the animation.
+
+        :param molecule_count: Number of molecules
+        :param size_of_hydrogen: This is an arbitrary radius for the animated sphere representing hydrogen atoms
+        :param size_ratio_oxygen_hydrogen: This ratio determines, how much larger an oxygen atom is compared to hydrogen
+        :return: Numpy-array of atom-specific radius-values
+        """
+
+        size_array = np.zeros(shape=(molecule_count * 3))
+
+        for i in range(molecule_count * 3):
+
+            if i % 3 == 0 or i == 0:
+
+                size_array[i] = size_of_hydrogen * size_ratio_oxygen_hydrogen
+
+            else:
+
+                size_array[i] = size_of_hydrogen
+
+        return size_array
+
+'''
+if __name__ == "__main__":
+
+    # defining initial values
+
+    hdf5_file_name_one = "data_3000.hdf5"
+    hdf5_file_name_two = "data_1000.hdf5"
+    hdf_file_t_fine = 'data.hdf5'
+
+    timesteps_one = 3000
+    timesteps_two = 1000
+    timesteps_fine = 10000
+    time_interval = 10
+    molecule_count_one = int(FileIO.return_molecule_count('data_3000.hdf5'))
+    molecule_count_two = int(FileIO.return_molecule_count('data_1000.hdf5'))
+    molecule_count_fine = int(FileIO.return_molecule_count('data.hdf5'))
+    # file creation for saving data
+    # NOTE: the created .txt file is for testing purposes only !
+
+
+    #FileIO.save_data(timesteps, time_interval, molecule_count, "data.hdf5")
+    #FileIO.write_hdf5_txt('data.hdf5')
+
+    # accessing the saved data and assigning the positional values to a numpy array
+
+    position_array_one = np.array(FileIO.extract_data_to_np_array(timesteps_one, time_interval, molecule_count_one, hdf5_file_name_one)[0])
+    velocity_array_one = np.array(FileIO.extract_data_to_np_array(timesteps_one, time_interval, molecule_count_one, hdf5_file_name_one)[1])
+
+    position_array_two = np.array(FileIO.extract_data_to_np_array(timesteps_two, time_interval, molecule_count_two, hdf5_file_name_two)[0])
+    velocity_array_two = np.array(FileIO.extract_data_to_np_array(timesteps_two, time_interval, molecule_count_two, hdf5_file_name_two)[1])
+
+    position_array_fine = np.array(FileIO.extract_data_to_np_array(timesteps_fine, time_interval, molecule_count_fine, hdf_file_t_fine)[0])
+    velocity_array_fine = np.array(FileIO.extract_data_to_np_array(timesteps_fine, time_interval, molecule_count_fine, hdf_file_t_fine)[1])
+
+    # for animation, the atom-specific colours are now defined
+
+    rgb_array_oxygen = np.array([255, 88, 32])
+    rgb_array_hydrogen = np.array([0, 96, 255])
+    colours = Animation.define_colour_array(molecule_count_fine, rgb_array_oxygen, rgb_array_hydrogen)
+
+    # for animation, the atom-specific sizes (based roughly on VDW-radii) are now defined
+
+    sizes_of_atoms = Animation.define_atom_size_array(molecule_count_fine, 49, 2)
+
+    # the plot and figure are created, using the data from the hdf5 file
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    title = ax.set_title("3D Animation of scatters representing atoms; \n\nTimestep: {0}, Time Interval: {1}".format(0, time_interval))
+    ax.set_xlabel("X - position")
+    ax.set_ylabel("Y - position")
+    ax.set_zlabel("Z - position")
+
+    # now the nice slicing of the positional array comes into play;
+    # the axes of the 3D-plot are scaled based on the maximum and minimum x, y and z - coordinates of all timesteps,
+    # meaning that the -simulated molecules will always stay in the animation box
+    # (not to be confused with the simulation box !)
+
+    # as an example:
+    # x_position_range = (np.amax(position_array[:][:][..., 0]), np.amin(position_array[:][:][..., 0]))
+    # this gives a tuple containing the maximum (np.amax()) and minimum (np.amin()) values
+    # of all available x-coordinates from all timesteps
+
+    x_position_range = (np.amin(position_array_fine[:][:][..., 0]), np.amax(position_array_fine[:][:][..., 0]))
+    y_position_range = (np.amin(position_array_fine[:][:][..., 1]), np.amax(position_array_fine[:][:][..., 1]))
+    z_position_range = (np.amin(position_array_fine[:][:][..., 2]), np.amax(position_array_fine[:][:][..., 2]))
+    ax.set_xlim(x_position_range)
+    ax.set_ylim(y_position_range)
+    ax.set_zlim(0, 5)
+
+    # create the graph with scatter points and animate the simulation using a repeated call to the update_graph method
+
+    graph = ax.scatter(position_array_fine[0][:][..., 0], position_array_fine[0][:][..., 1], position_array_fine[0][:][..., 2],
+                       c=colours/255, s=sizes_of_atoms)
+
+    update_interval_animation = 10 # in milliseconds
+
+    anim_func = animation.FuncAnimation\
+        (
+            fig,
+            Animation.update_graph,
+            timesteps_fine // time_interval,
+            fargs=
+            (
+                timesteps_fine,
+                position_array_fine,
+                time_interval,
+                ax,
+                update_interval_animation
+            ),
+            interval=update_interval_animation,
+            blit=False
+        )
+
+    # finally show the animation
+
+    plt.show()
+
+    # show the trajectory of the first hydrogen atom in x - direction
+
+    fig = plt.figure()
+    fig.add_subplot()
+
+    plt.plot(position_array_fine[:][:, 1][..., 0], velocity_array_fine[:][:, 1][..., 0])
+    plt.xlabel('X - coordinate')
+    plt.ylabel('Velocity in X - direction')
+
+    plt.show()
+
+    # show the trajectory of the first hydrogen atom in y - direction
+
+    plt.plot(position_array_fine[:][:, 1][..., 1], velocity_array_fine[:][:, 1][..., 1])
+    plt.xlabel('Y - coordinate')
+    plt.ylabel('Velocity in Y - direction')
+
+    plt.show()
+
+    # show the trajectory of the first hydrogen atom in z - direction
+
+    plt.plot(position_array_fine[:][:, 1][..., 2], velocity_array_fine[:][:, 1][..., 2])
+    plt.xlabel('Z - coordinate')
+    plt.ylabel('Velocity in Z - direction')
+
+    plt.show()
+    '''
---
+++ molecular_simulation_python_files/cell_linked_list.py
@@ -0,0 +1,246 @@
+import numpy as np
+import math
+from scipy.spatial import distance
+from periodic_kdtree import PeriodicCKDTree
+from pandas.core.common import flatten
+
+class CellLinked ():
+
+    def __init__ (self, box_len: float, r_cut: float, postate : np.ndarray) :
+        """
+        Initialize parameters for identified neighbor cells and particles inside these cells
+
+            Parameters:
+                box_len (float) : cube simulation box dimension (box_len*box_len*box_len)
+                r_cut (float) : cut-off radius
+                no_cells (float) : number of cell per axis
+                postate (np.ndarray): position of particles in 3D
+        """
+        self.box_len = box_len
+        self.r_cut = r_cut
+        self.no_cells = math.floor (self.box_len/self.r_cut)
+        self.postate = postate
+
+        # Initialize a Head array with -1, size = the total number of cells
+        self.head_arr = [-1] * self.no_cells*self.no_cells*self.no_cells
+
+        # Initialize a list array with -1, size = the total number of particles
+        self.list_arr = [-1] * self.postate.shape[0]
+
+    def __call__ (self) -> tuple:
+
+        """ 
+            Compute the head and list array using simulation box length, cut-off radius, and the position state.
+
+            Returns
+                head_arr (list):   head array in the linked list (head_arr.shape==number of cells)
+                list_arr (list):   list array in the linked list (list_arr.shape==number of particles)
+        """
+
+        for i in range (0, self.postate.shape[0]) :
+
+            # calculate cell index of each particles based on the particle position
+            index = self._cellindex (self.postate[i,:])
+
+            # calculate list array
+
+            self.list_arr [i] = self.head_arr [index]
+
+            # calculate head array         
+            self.head_arr [index] = i
+
+        return self.head_arr, self.list_arr
+
+
+            
+
+
+    def _cellindex (self, one_particle_pos : np.ndarray) -> int :
+
+        """ Calculate the cell index based on the i, j, and k index
+
+                Parameter :
+                    one_particle_pos (np.ndarray) : position of 1 particle (one_particle_pos.shape = (1,3))
+                
+                Return :
+                    cell's index (int) : cell index for each particles 
+
+        """
+        i = math.floor (one_particle_pos[0]/self.r_cut)
+        j = math.floor (one_particle_pos[1]/self.r_cut)
+        k = math.floor (one_particle_pos[2]/self.r_cut)
+        return int (k+j*self.no_cells+i*self.no_cells*self.no_cells)
+
+    def _find_neighbor_index (self, cellind: int) -> list:
+
+        """
+        calculate neighbor cell index based on the cell index
+            Parameter :
+                cellind (int) : cell index which is camputed by _cellindex method
+
+            Return :
+                neighbor_cell_inx (list) : neighbor cell indexes by the given cell index
+
+        """
+        #  calculate i, j, and k index based on the cell index
+
+        i_counter = math.floor (cellind/self.no_cells**2)
+        j_counter = math.floor ((cellind - i_counter*self.no_cells**2)/self.no_cells)
+        k_counter = math.floor (cellind - j_counter*self.no_cells - i_counter*self.no_cells**2)
+      
+
+        # calculate neighbor cell index for each cell index
+        index = [-1,0,1]
+        neighbor_cell_inx = []
+        for ii in index :
+            for jj in index :
+                for kk in index :
+                    if 0 <= ii + i_counter < self.no_cells and 0 <= jj + j_counter < self.no_cells and 0 <= kk + k_counter < self.no_cells :
+                        new_index = (kk + k_counter) + (jj + j_counter) *self.no_cells + (ii + i_counter) *self.no_cells*self.no_cells
+                        neighbor_cell_inx.append(new_index)
+        return neighbor_cell_inx
+
+    def _find_neighbor_cells (self, head_arr: list) -> list:
+
+        """
+        calculate neighbor cells based on head array
+
+            Parameter :
+                head_arr (list) : a list of head array
+
+            Return :
+                neighbor_cells (list) : neighbor cell indexes
+        """
+
+        neighbor_cell_indexes = []
+        for j in range (0, len (head_arr)) :           
+            neighbor_cell_inx = self._find_neighbor_index (j)           
+            neighbor_cell_indexes.append(neighbor_cell_inx)
+
+        return neighbor_cell_indexes
+    
+    def _find_particles_inside_neighbor_cells (self, list_arr : list, head_arr : list, neighbor_cell_indexes : list) -> np.ndarray:
+    
+        """
+        find particles in each cell and neighbor cells which are identified
+            Parameters :
+                list_arr (list) : list array
+                head_arr (list) : head array
+                neighbor_cell_indexes (list) : neighbor cells indexes
+
+            Return :
+                temporary_state (np.ndarray) : state of all particles positioned on the given neighbor_cell_indexes
+        """
+        new_head_arr =[]
+        for i in neighbor_cell_indexes :
+            new_head_arr.append (head_arr[i])
+        temporary_state = np.zeros((1,3))
+
+        for ii in new_head_arr:
+            if ii > -1 :
+                cell_state = np.zeros((1,3))
+                cell_state = np.concatenate((cell_state, np.array ([self.postate[ii]])))
+                while  list_arr[ii] != -1 :
+                    cell_state = np.concatenate((cell_state, np.array ([self.postate[list_arr[ii]]])))
+                    ii = list_arr[ii]
+
+                cell_state = np.flip(cell_state[1:, :], 0)
+                temporary_state = np.concatenate((temporary_state, cell_state))
+        
+        
+        return temporary_state[1:, :]
+
+
+
+class CellLinkedPeriodic ():
+
+    def __init__ (self, box_len: float, r_cut: float, postate : np.ndarray, target_position: np.ndarray) :
+
+        self.box_len = box_len
+        self.r_cut = r_cut
+        self.postate = postate
+        self.target_position = target_position
+
+    def __call__ (self) :
+
+        return self._find_particles_inside_neighbor_cells()
+        
+    
+    def _find_particles_inside_neighbor_cells (self) -> np.ndarray:
+         
+        bounds = np.array([self.box_len, self.box_len, self.box_len])
+
+        # Build kd-tree
+        tree = PeriodicCKDTree(bounds, self.postate)
+
+        # Find neighbors within a fixed distance of a point
+        neighbors = tree.query_ball_point(self.target_position, r=self.r_cut)
+
+        new_state = np.take(self.postate, neighbors, axis=0)
+        
+        return new_state
+
+         
+
+class FindParticleDistance () :
+
+    def __init__ (self, target_particle : np.ndarray, temporary_state : np.ndarray, r_cut: float) :
+
+        self.target_particle = target_particle
+        self.temporary_state = temporary_state
+        self.r_cut = r_cut
+
+    def __call__ (self) :
+
+        return self._find_particles_distance_inside_neighbor_cells() 
+    
+    def _find_particles_distance_inside_neighbor_cells(self) :
+
+        '''
+
+        '''
+        dists = distance.cdist(self.temporary_state, self.target_particle.reshape(1,3), 'euclidean')
+        a = dists < self.r_cut
+        true_state = self.temporary_state[a[:, 0]]
+        return true_state
+
+
+
+
+if __name__=="__main__":
+
+    box_len=12
+    r_cut= 6
+    postate = box_len * np.random.random_sample((24, 3))
+    print (postate.shape)
+
+    effective_state = CellLinked (box_len, r_cut, postate)
+    result = effective_state()
+    print (result)
+
+    #target_particle = np.array([0, 0, 0]).reshape(1,3)
+    #model = FindParticleDistance (target_particle, postate, 1.5)
+    #result = model._find_particles_distance_inside_neighbor_cells()
+    #print (result)
+    #head_arr, list_arr = model ()
+    #periodic_neighbor_index = model._find_neighbor_cells ()
+    #print (periodic_neighbor_index)
+
+
+
+
+    #print (len(list_arr))
+    #print (len(head_arr))
+    #neighbor_cells = model._find_neighbor_cells(head_arr)
+    #neighbor_cells = neighbor_cells[13]
+    #temporary_state = model._find_particles_inside_neighbor_cells (list_arr, head_arr, neighbor_cells)
+
+    #print (temporary_state.shape)
+    #print (temporary_state)
+    #mask = np.isin(postate, temporary_state)
+    #assert np.all(mask == True) == True, 'particles in neighbor cells has been indetified wrongly'
+
+    #print ("the head array is : \n ", head_arr)
+    #print ("list array is : \n ", list_arr)
+
+    
