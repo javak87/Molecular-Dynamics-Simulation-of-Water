527c4c2d3fb19b4ac5f1f28b2c2725482511f778
Florian <florian.anders@fu-berlin.de>
2021-02-28 22:37:38 +0100

    updated setup.py with information

--- setup.py
+++ setup.py
@@ -1,6 +1,25 @@
-# 28.02.2021
+# 26.02.2021
 # Author: Florian Anders
 
-import numpy as np
-import pytest
-import typing
+from setuptools import setup
+
+setup\
+    (
+        name='molecular_simulation_water_semester_project',
+        version='1.0',
+        author='Javad Kasravi, Hongmiao Wu, Cenk Üstündag, Muhtasim Fuad, Dennis Mattutat, Florian Anders',
+        author_email='javad.kasravi@fu-berlin.de, hongmiao.wu@fu-berlin.de, cenku95@zedat.fu-berlin.de, muhtasif71@zedat.fu-berlin.de, florian.anders@fu-berlin.de',
+        packages=['molecular_simulation_water'],
+        description='Package containing python files for the molecular dynamics simulation of water',
+        install_requires=
+        [
+            "numpy",
+            "scipy",
+            "matplotlib",
+            "h5py",
+            "pycallgraph",
+            "pandas",
+            "typing",
+            "transformations",
+        ],
+    )
